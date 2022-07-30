fbcfc48cbe4d5ccf9636704db07ba9ebc7a32e31
AndrewYoung72 <97897877+AndrewYoung72@users.noreply.github.com>
2022-07-30 13:34:34 -0600

    homepage styling

--- client/src/components/MapView.js
+++ client/src/components/MapView.js
@@ -5,7 +5,6 @@ import "./index.css";
 const MapView = ({ positions }) => {
   return (
     <>
-      <h1>Hello world</h1>
       <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -24,17 +24,13 @@ export default function Home() {
           <p className="title">Spokes People</p>
         </div>
       </section>
-      <section className="section is-medium has-background-info-light">
-        <h2 className="subtitle">Search Input</h2>
-        <input className="input is-info column is-one-fifth" type="text" placeholder="Info input"></input>
-        <input className="input is-info column is-one-fifth" type="text" placeholder="Info input"></input>
-      </section>
+     
       <div className="container">
         <div className="title is-parent">
-          <article className="title is-child notification is-info">
+          <article className="title is-child notification is-info-light">
             <div className="content">
-              <p className="title ">Search Map</p>
-              <p className="subtitle">Add map</p>
+              <p className="title ">City Search</p>
+              <input className="input is-info column is-one-fifth" type="text" placeholder="City"></input>
               <div class="content"> <MapView positions={positions} /></div>
             </div>
           </article>
