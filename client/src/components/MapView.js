50cab57eacc3aaa7dbf90f1b338b7a2f491c4cf6
Brian Ward <brianwardcs@gmail.com>
2022-08-02 20:48:10 -0600

    marker progress

--- client/src/components/MapView.js
+++ client/src/components/MapView.js
@@ -1,12 +1,26 @@
-import { MapContainer, TileLayer, Marker, Popup, } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import LocationMarker from "./LocationMarker";
 import "./index.css";
 
+/*
+
+positions = [
+  {
+    name: <name>
+    location:  {
+      latitude: <lat>
+      longitude: <lon>
+    }
+  }
+]
+
+
+**/
 
 const MapView = ({ positions }) => {
   return (
     <>
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
+      <MapContainer center={[37.783870, -99.044231]} zoom={5} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -14,21 +28,19 @@ const MapView = ({ positions }) => {
         {positions.map((position) => (
           <Marker
             position={[position.location.latitude, position.location.longitude]}
-            // icon={Icon} 
+            // icon={Icon}
+            key={`marker-${position.name}`}
           >
-           
-            <Popup>
-              
+            <Popup key={`popup-${position.name}`}>
               {position.name}
               <br />
-              {position.location.city}
+              {/* {position.location.city} */}
               <br />
               {position.location.latitude}
               <br />
               {position.location.longitude}
             </Popup>
           </Marker>
-          
         ))}{" "}
       </MapContainer>
     </>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,22 +1,69 @@
-import React, {useState, useEffect} from "react";
-import MapView from "./MapView";
+import React, { useState, useEffect } from "react";
+// import MapView from "./MapView";
 import "bulma/css/bulma.min.css";
-// import MapView from "../components/MapView";
+import MapView from "../components/MapView";
 
 export default function Home() {
   const [positions, setPositions] = useState([]);
 
-  // useEffect(() => {
-  //   names()
-  // }, [])
-  
-  const names = async () => {
-    const responce = await fetch('http://api.citybik.es/v2/networks/?rapidapi-key=f17db1dcddmsh3dce12092ca5ebep17ef72jsn4fcd0fdb0eeb')
-  const json = await responce.json();
-  console.log(json);
-    setPositions(json?.networks)
-  }
-  
+  useEffect(() => {
+    var api = "http://api.citybik.es/v2/networks/";
+    fetch(api)
+      .then((response) => {
+        return response.json();
+      })
+      .then(function (response) {
+        var networks = response.networks;
+        var usNetworks = networks.filter(
+          (network) => network.location.country === "US"
+        );
+        // console.log(usNetworks);
+        var newApi = "http://api.citybik.es";
+        let tempPositions = [];
+        for (var i = 0; i < usNetworks.length; i++) {
+          const href = usNetworks[i].href;
+          // const city = usNetworks[i].location.city;
+          // const lat = usNetworks[i].location.latitude;
+          // const lon = usNetworks[i].location.longitude;
+          // console.log(`The city is ${city} lat is ${lat} lon is ${lon}`);
+          fetch(newApi + href)
+            .then((data) => {
+              return data.json();
+            })
+            .then(function (data) {
+              var station = data.network.stations;
+              // console.log(station);
+              for (var j = 0; j < station.length; j++) {
+                var name = station[j].name;
+                // var emptySlots = station[j].empty_slots;
+                // var availableBikes = station[j].free_bikes;
+                var latitude = station[j].latitude;
+                var longitude = station[j].longitude;
+                // console.log(name,latitude,longitude);
+
+                let position = {
+                  name,
+                  location: {
+                    latitude,
+                    longitude,
+                  }
+                }
+
+                tempPositions.push(position);
+                // console.log(`${availableBikes} bikes at ${name}`);
+                // console.log(`${emptySlots} slots available at ${name}`);
+              }
+            });
+        }
+        let tpSlice = tempPositions.slice(0,100);
+        console.log(tpSlice);
+        return tpSlice;
+      }).then((tp) => {
+        setPositions(tp);
+        console.log(positions);
+      });
+  }, []);
+
   return (
     <div>
       <section className="hero has-background-info is-align-items-center">
@@ -24,14 +71,21 @@ export default function Home() {
           <p className="title has-text-light">Spokes People</p>
         </div>
       </section>
-     
+
       <div className="container">
         <div className="title is-parent">
           <article className="title is-child notification is-info-light">
             <div className="content">
               <p className="title ">City Search</p>
-              <input className="input is-info column is-one-fifth" type="text" placeholder="City"></input>
-              <div class="content"> <MapView positions={positions} /></div>
+              <input
+                className="input is-info column is-one-fifth"
+                type="text"
+                placeholder="City"
+              ></input>
+              <div class="content">
+                {" "}
+                <MapView positions={positions} />
+              </div>
             </div>
           </article>
         </div>
@@ -41,8 +95,14 @@ export default function Home() {
           <p>
             CSS by
             <strong className="has-text-light"> Bulma</strong> site by{" "}
-            <a className="has-text-primary" href="https://github.com/bward3/spokes-people"> Group 4-Project 3</a>. July
-            2022.
+            <a
+              className="has-text-primary"
+              href="https://github.com/bward3/spokes-people"
+            >
+              {" "}
+              Group 4-Project 3
+            </a>
+            . July 2022.
           </p>
         </div>
       </footer>
