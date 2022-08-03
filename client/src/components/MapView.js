4fc09bb3b977dfd7f795328fd12ee9ca0d68bb91
Norman Herrera <norman.herrera@ymail.com>
2022-08-03 14:46:14 -0700

    comments

--- client/src/components/MapView.js
+++ client/src/components/MapView.js
@@ -4,35 +4,35 @@ import "./index.css";
 
 function MapView() {
   let bikes = [];
-  fetch('http://api.citybik.es/v2/networks/')
-  .then(res => res.json())
+  fetch('http://api.citybik.es/v2/networks/') 
+  .then(res => res.json()) // response to json
   .then(function (response) {
-    var networks = response.networks;
-    var usNetworks = networks.filter((network) => network.location.country === 'US');
+    var networks = response.networks; 
+    var usNetworks = networks.filter((network) => network.location.country === 'US'); // filters of US networks
     var api = "http://api.citybik.es";
     for (var i = 0; i < usNetworks.length; i++) {
-      const href = usNetworks[i].href;
-      fetch(api + href)
+      const href = usNetworks[i].href; // gets individual hrefs
+      fetch(api + href) 
         .then(data => data.json())
         .then(function (data) {
           var station = data.network.stations;
-          for (var j = 0; j < station.length; j++) {
+          for (var j = 0; j < station.length; j++) { //inner loop to search of individual stations
             var name = station[j].name;
             var lat = station[j].latitude;
             var lon = station[j].longitude;
             var emptySlots = station[j].empty_slots;
             var availableBikes = station[j].free_bikes;
-            let info = {
+            let info = { //takes infor to pass on
               name, 
               location: { lat, lon },
               emptySlots,
               availableBikes
             };
-            bikes.push(info);
+            bikes.push(info); // sends the info to our original empty array
           }
           return bikes;
         })
-      } console.log(bikes);
+      } console.log(bikes); // this works, populated with 8,000 objects
   })
 
   return (
